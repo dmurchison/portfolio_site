89121b7edee6def93dbf7ea963a4d4f5e6db2678
Duncan Murchison <duncancmurchison@gmail.com>
2023-01-04 17:04:38 -0500

    main commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,19 @@
-
-
 // Scripts
 
 window.addEventListener('DOMContentLoaded', event => {
-
   // Navbar shrink function
   var navbarShrink = function () {
     const navbarCollapsible = document.body.querySelector('#mainNav');
-
     if (!navbarCollapsible) {
       return;
     }
-
     if (window.scrollY === 0) {
       navbarCollapsible.classList.remove('navbar-shrink')
     } else {
       navbarCollapsible.classList.add('navbar-shrink')
     }
   };
-
+  
   // Shrink the navbar 
   navbarShrink();
   // Shrink the navbar when page is scrolled
